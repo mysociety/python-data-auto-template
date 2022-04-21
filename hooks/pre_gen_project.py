d6832f1c820bbf861816c3313c3bccc1a7d7189f
Alex Parsons <alex.parsons@mysociety.org>
2022-04-21 16:36:01 +0100

    Update references for new version of cookie data repo

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -9,12 +9,27 @@ if any(x in template_dir for x in ["https:", "gh:"]):
 else:
     template_dir = Path(template_dir)
 
+
+def amend_file(filepath: Path, replace: dict = {}):
+    with open(filepath, "r") as f:
+        txt = f.read()
+    for key, value in replace:
+        txt = txt.replace(key, value)
+    with open(filepath, "w") as f:
+        f.write(txt)
+
+    filename = str(filepath)
+    for key, value in replace:
+        filename = filename.replace(key, value)
+    if str(filepath) != filename:
+        filepath.rename(filename)
+
 repo_dir = template_dir / ("{" + "{ cookiecutter.repo_name }" + "}")
 
-template_repo = "https://github.com/mysociety/template_notebook"
+template_repo = "https://github.com/mysociety/template_data_repo"
 template_branch = "main"
 
-helper_repo = "https://github.com/mysociety/notebook_helper"
+helper_repo = "https://github.com/mysociety/data_common"
 helper_branch = "main"
 
 os.system(f"cd {template_dir} && git submodule update --init --recursive")
@@ -24,8 +39,8 @@ os.system(f'cd "{repo_dir}" && git reset --hard')
 os.system(f'cd "{repo_dir}" && git remote rm origin')
 os.system(f'cd "{repo_dir}" && git remote add origin "{template_repo}" && git fetch origin && git pull origin main && git checkout main')
 
-os.system(f'cd "{repo_dir}" && cd notebook_helper && git remote rm origin')
-os.system(f'cd "{repo_dir}" && cd notebook_helper && git remote add origin "{helper_repo}" && git fetch origin && git pull origin main && git checkout main')
+os.system(f'cd "{repo_dir}" && cd src/data_common && git remote rm origin')
+os.system(f'cd "{repo_dir}" && cd src/data_common && git remote add origin "{helper_repo}" && git fetch origin && git pull origin main && git checkout main')
 
 source_readme = Path(template_dir, "cookie-readme.md")
 dest_readme = Path(repo_dir, "readme.md")
@@ -33,3 +48,15 @@ general_readme = Path(repo_dir, "notebooks-readme.md")
 
 print(f"Copying {source_readme} to {dest_readme}")
 shutil.copyfile(source_readme, dest_readme)
+
+# Amend files that have a direct reference to the original name
+
+replace = {"template_data_repo": "{" + "{ cookiecutter.repo_name }" + "}",
+            "description": "{" + "{ cookiecutter.description }" + "}"}
+
+amend_file(Path(repo_dir, "pyproject.toml"), replace )
+amend_file(Path(repo_dir, "docker-compose.yml"), replace )
+amend_file(Path(repo_dir, "Dockerfile.dev"), replace )
+amend_file(Path(repo_dir, "Dockerfile"), replace )
+amend_file(Path(repo_dir, "tests", "test_template_data_repo.py"), replace )
+
